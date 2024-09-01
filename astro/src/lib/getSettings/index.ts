b5ce35e5f2e3f4515b2e5ef41444c4ce8eef44da
Mike Newberry <hello@mikenew.xyz>
2024-09-01 22:22:03 +0100

    add generated sanity types

--- astro/src/lib/getSettings/index.ts
+++ astro/src/lib/getSettings/index.ts
@@ -1,18 +1,13 @@
+import type { SettingsSingleton } from "../../types/sanity.types";
 import client from "../client";
 
-export type Settings = {
-	siteTitle: string;
-	siteDescription: string;
-	siteKeywords: string;
-};
-
-async function getSettings(): Promise<Settings | undefined> {
+async function getSettings(): Promise<SettingsSingleton | undefined> {
 	try {
 		const dataArray = await client.fetch('*[_type == "settingsSingleton"]');
 
 		if (!dataArray || dataArray.length === 0) throw new Error("Failed to fetch settings.");
 
-		const data: Settings = dataArray[0];
+		const data: SettingsSingleton = dataArray[0];
 
 		return data;
 	} catch (error) {
