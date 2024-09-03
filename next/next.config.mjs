1a5741bf36ac556a2e7433056a4a1d39e40c7916
Mike Newberry <hello@mikenew.xyz>
2024-09-03 15:10:12 +0100

    update next config

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "export",
+	basePath: "/example-blog",
 	images: {
+		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: "https",
